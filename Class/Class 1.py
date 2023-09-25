8ebdb7aa5d5a32f9911e1cce53900c81b215191a
Johnse <JOHND3DS@GMAIL.COM>
2023-09-24 22:12:35 -0500

    carga

---
+++ Class/Class 1.py
@@ -0,0 +1,48 @@
+# Sma
+# Variables
+a = 5
+b = 3
+Sma = a+b
+print(Sma)
+# Resta
+rest = a-b
+print(rest)
+# multipli}
+multi = a*b
+print(multi)
+# factorizacion
+facto = a**b
+print(facto)
+# division
+div = a/b
+print(div)
+# division Entera Olvida los decimales
+div2 = a//b
+print(div2)
+# modulo aproximacion de acuerdo a los decimales que se tienen
+mod = a % b
+print(mod)
+# Operador De Asignacion
+#Explicacion Dependiendo el tipo de operador este tomara el valor iniciaal actuara el operador y regreseara el resultado
+contador = 0
+contador -= 1
+print(contador)
+# Operador De Comparacion "==, != ,<>", comparar datos imprimen falso o verdadero
+#Igual
+a=5
+b=4
+resultado = a==b
+print(resultado)
+#Desigual
+result= a != b
+print (result)
+#Mayor que 
+resultad = a>b
+print(resultad)
+#Menor que
+resulta= a<b
+print (resulta)
+#Mayor o Igual que 
+resultad = a >= b
+print(resultad)
+
