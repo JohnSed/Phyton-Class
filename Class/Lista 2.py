e19e8b66ab44e0554d9d822e521aae93214025bb
Johnse <JOHND3DS@GMAIL.COM>
2023-10-11 22:28:49 -0500

    listas

---
+++ Class/Lista 2.py
@@ -0,0 +1,12 @@
+corredores= ["Colombia", "Mexicano", "Ecuatoriano"] #Lista - Son mutables siempre separar itemsn en comillas y luego en coma
+print (corredores)
+print (type (corredores))#Imprimir 
+corredores2= ("Juan", "Jose", "Carlos") #Esto es una tupla - no mutables es decir no se puede modificar, guarda la informacion como voy agregando
+#mirar item
+print (corredores[0])#imprimir ya sea dupla o lista
+pueblos ={"Retiro", "Vijes", "Yotoco", "Yotoco"}#Set, en los conjuntos no se guarda como se agregar
+print (pueblos)
+#Creacion Diccionarios, CLAVE Y LUEGO VALOR
+Capitales={"Colombia":"Bogota", "Japon": "Tokyio", "Mexico":"DC"}
+print(type(Capitales))
+print (Capitales["Colombia"])
